e78d4f8c9c792ff124b545d7ab3e86e2fdc722ce
Stefan von der Krone <stefanvonderkrone@gmx.de>
2015-01-08 12:25:40 +0100

    Leaf now has isLeaf set to true Branch now has isBranch set to true added functions map, filter, each and flatten

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ function Stats( path, stats ) {
 Stats.prototype = {
     constructor: Stats,
     path: "",
+    name: "",
     stats: undefined
 };
 
 function wrapStat( path ) {
     return function( stats ) {
         return new Stats( path, stats );
-    }
+    };
 }
 
 function lstat( path ) {
@@ -35,13 +36,92 @@ function listDirectory( path ) {
     return q.nfcall( fs.readdir, path );
 }
 
+function filterTree( cb, tree ) {
+    if ( tree.isLeaf ) {
+        return cb( tree.content ) ? new Leaf( tree.content ) : undefined;
+    } else if ( tree.isBranch ) {
+        if ( cb( tree.content ) ) {
+            var children;
+            children = _.chain( tree.children )
+                .map( function( child ) {
+                    return filterTree( cb, child );
+                } )
+                .filter( function( child ) {
+                    return !!child;
+                } )
+                .value();
+            if ( children.length === 0 ) {
+                return new Leaf( tree.content );
+            }
+            return new Branch( tree.content, children );
+        } else {
+            return undefined;
+        }
+    } else {
+        throw new Error( "no tree" );
+    }
+}
+
+function mapTree( cb, tree ) {
+    if ( tree.isLeaf ) {
+        return new Leaf( cb( tree.content ) );
+    } else if ( tree.isBranch ) {
+        return new Branch( cb( tree.content ), _.map( tree.children, function( child ) {
+            return mapTree( cb, child );
+        } ) );
+    } else {
+        throw new Error( "no tree" );
+    }
+}
+
+function eachTree( cb, tree ) {
+    if ( tree.isLeaf ) {
+        cb( tree.content );
+    } else if ( tree.isBranch ) {
+        cb( tree.content );
+        _.each( tree.children, function( child ) {
+            eachTree( cb, child );
+        } );
+    } else {
+        throw new Error( "no tree" );
+    }
+}
+
+function flattenTree( tree ) {
+    if ( tree.isLeaf ) {
+        return [ tree.content ];
+    } else if ( tree.isBranch ) {
+        return [ tree.content ]
+            .concat( _.chain( tree.children )
+                .map( flattenTree )
+                .flatten()
+                .value()
+            );
+    } else {
+        throw new Error( "no tree" );
+    }
+}
+
 function Tree() {}
 
 Tree.prototype = {
     constructor: Tree,
     isLeaf: false,
     isBranch: false,
-    content: undefined
+    content: undefined,
+    children: undefined,
+    filter: function( cb ) {
+        return filterTree( cb, this );
+    },
+    map: function( cb ) {
+        return mapTree( cb, this );
+    },
+    each: function( cb ) {
+        return eachTree( cb, this );
+    },
+    flatten: function() {
+        return flattenTree( this );
+    }
 };
 
 function Leaf( content ) {
@@ -49,7 +129,8 @@ function Leaf( content ) {
 }
 
 Leaf.prototype = _.assign( new Tree(), {
-    constructor: Leaf
+    constructor: Leaf,
+    isLeaf: true
 } );
 
 function Branch( content, children ) {
@@ -59,16 +140,9 @@ function Branch( content, children ) {
 
 Branch.prototype = _.assign( new Tree(), {
     constructor: Branch,
-    children: []
+    children: [],
+    isBranch: true
 } );
-
-//Tree.createLeaf = function( content ) {
-//
-//};
-//
-//Tree.createBranch = function( contents ) {
-//
-//};
 
 function handleStat( stats ) {
     if ( stats.stats.isDirectory() ) {
@@ -77,6 +151,10 @@ function handleStat( stats ) {
                 return q.all(_.map( files, function( file ) {
                     var path = stats.path + "/" + file;
                     return lstat( path )
+                        .then( function( stats ) {
+                            stats.name = file;
+                            return stats;
+                        } )
                         .then( handleStat );
                 } ) );
             } )
--- test.js
+++ test.js
@@ -7,6 +7,18 @@
 
 require( "./index.js" )( process.cwd() )
     .then( function( fsTree ) {
+        fsTree = fsTree
+            .filter( function( content ) {
+                return content.stats.isDirectory();
+            } )
+            .map( function( content ) {
+                return content.path;
+                //return {
+                //    path: content.path,
+                //    name: content.name
+                //};
+            } )
+            .flatten();
         console.log( JSON.stringify( fsTree, undefined, "  " ) );
     }, function( err ) {
         console.log( "Error:", err );
